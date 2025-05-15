44a36f6e19f951f0694197d81e6284378ea3aea1
Isma <2023_Ismael.younes@iticbcn.cat>
2025-05-15 20:21:09 +0200

    finalitzat

---
+++ demo/src/main/java/com/example/demo/Controladors/StudentController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RequestMapping(value = "/api/v1/student")
+@RestController
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @PostMapping("/save")
+    public Mono<Student> save(@RequestBody Student student) {
+        return studentRepository.save(student);
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Student> findById(@PathVariable String id) {
+        return studentRepository.findById(id);
+    }
+
+    @GetMapping("/all")
+    public Flux<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    @PutMapping("/update")
+    public Mono<Student> update(@RequestBody Student student) {
+        return studentRepository.save(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> delete(@PathVariable String id) {
+        return studentRepository.deleteById(id);
+    }
+}
