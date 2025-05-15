5104ef9e32eb0ea298cb48eaf95ffec092caaffc
PauSintesP <pausintespaul@gmail.com>
2025-05-15 20:35:37 +0200

    activitat a mitges, com la vaig deixar a ella :C, falta fer el service Implement

--- demo/src/main/java/com/example/demo/Serveis/StudentServiceImpl.java
+++ demo/src/main/java/com/example/demo/Serveis/StudentServiceImpl.java
@@ -3,9 +3,7 @@ package com.example.demo.Serveis;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.DTO.StudentDTO;
-import com.example.demo.Mapper.StudentMapper;
 import com.example.demo.Model.Student;
-import com.example.demo.Repositoris.StudentsRepo;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
@@ -13,7 +11,39 @@ import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
-public class StudentServiceImpl implements StudentService {
+public class StudentServiceImpl implements StudentService {@Override
+    public Mono<Student> save(StudentDTO studentDTO) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public Mono<Student> findById(String id) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public Flux<Student> findAll() {
+
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Mono<Student> update(StudentDTO studentDTO) {
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public Mono<Void> delete(String id) {
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public Flux<Student> findByNameRegex(String namePattern) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'findByNameRegex'");
+    }
     
     //falta fer els service Pau SIntes controla. :)
 }
