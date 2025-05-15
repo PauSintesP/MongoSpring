9ae9129bce77bf2ef84a1f126479b764bdc3ff74
PauSintesP <pausintespaul@gmail.com>
2025-05-15 20:07:52 +0200

    activity

--- demo/src/main/java/com/example/demo/DTO/StudentDTO.java
+++ demo/src/main/java/com/example/demo/DTO/StudentDTO.java
@@ -1,18 +1,14 @@
 package com.example.demo.DTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 
-public class StudentDTO {
-    private String name;
-    private String lastname1;
-    private String gender;
-    private String email;
-    private int birth_year;
-    private String phone;
+public record StudentDTO(
+     String name;
+     String lastname1;
+     String gender;
+     String email;
+     int birth_year;
+     String phone;
+){
+
 }
